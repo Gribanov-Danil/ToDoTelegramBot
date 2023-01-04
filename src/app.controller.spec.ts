2c84f8e3dd14c5a1e821fb4ad6f274b9085405eb
Danil Gribanov <99828828+Gribanov-Danil@users.noreply.github.com>
2023-01-04 23:41:03 +0700

    feat: Сделал вывод списка дел пользователя

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,4 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test, TestingModule } from "@nestjs/testing"
 import { AppUpdate } from "./app.update"
 import { AppService } from "./app.service"
 
--- src/app.update.ts
+++ src/app.update.ts
@@ -1,12 +1,30 @@
 import { AppService } from "./app.service"
-import { InjectBot, Start, Update } from "nestjs-telegraf"
+import { Hears, InjectBot, Start, Update } from "nestjs-telegraf"
 import { Context, Telegraf } from "telegraf"
 import { actionButtons } from "./app.buttons"
 
+const todos = [
+  {
+    id: 1,
+    name: "Помыть кота",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    name: "Покушать",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    name: "Поспать",
+    isCompleted: true,
+  },
+]
 @Update()
 export class AppUpdate {
   constructor(
-    @InjectBot() private readonly bot: Telegraf<Context>,
+    @InjectBot()
+    private readonly bot: Telegraf<Context>,
     private readonly appService: AppService,
   ) {}
 
@@ -14,5 +32,14 @@ export class AppUpdate {
   async startCommand(context: Context) {
     await context.reply("Привет, брат 🐺")
     await context.reply("Что хочешь сделать, брат?", actionButtons())
+  }
+
+  @Hears("📔 Список дел")
+  async getAll(context: Context) {
+    await context.reply(
+      `Ваш список дел: \n${todos
+        .map((action) => (action.isCompleted ? "✅" : "❌") + action.name + "\n")
+        .join("")}`,
+    )
   }
 }
