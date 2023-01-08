095ed85112f16169505f7dbd6df997469db082a6
Danil Gribanov <99828828+Gribanov-Danil@users.noreply.github.com>
2023-01-09 02:55:59 +0700

    feat: настроил TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppUpdate } from "./app.update"
 import { AppService } from "./app.service"
 import { TelegrafModule } from "nestjs-telegraf"
 import * as LocalSession from "telegraf-session-local"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { join } from "path"
 
 const sessions = new LocalSession({ database: "session_db.json" })
 @Module({
@@ -10,6 +12,17 @@ const sessions = new LocalSession({ database: "session_db.json" })
     TelegrafModule.forRoot({
       middlewares: [sessions.middleware()],
       token: "5720178480:AAFR6O5nWZMxBGUe-OvbXNWIp1EYXwBxEp0",
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5433,
+      database: "todo-app-bot",
+      username: "postgres",
+      password: "",
+      entities: [join(__dirname, "**", "*.entity.{ts,js}")],
+      migrations: [join(__dirname, "**", "*.entity.{ts,js}")],
+      synchronize: true,
     }),
   ],
   providers: [AppService, AppUpdate],
