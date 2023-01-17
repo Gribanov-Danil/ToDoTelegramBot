64f57df382171e122a1e48e507df68328731bdfa
Danil Gribanov <gribanovwork@yandex.ru>
2023-01-17 18:15:21 +0500

    feat: сделал entity для задачи

---
+++ src/task.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity({ name: "Task" })
+export class TaskEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+  @Column({ type: "text" })
+  name: string
+  @Column()
+  isCompleted: boolean
+}
